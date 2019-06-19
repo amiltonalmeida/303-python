5f400c634bf74f086e4dba84a8279846fb116c74
Amilton <amiltonfelipedc@gmail.com>
2019-06-19 16:08:59 -0300

    Exercícios de Python

---
+++ 303/python.py
@@ -0,0 +1,27 @@
+# nome = "Eric"
+# n_trab = 412
+# valor_hora = 18.25
+
+# if n_trab*valor_hora > 998 :
+#     print("O funcionario {} ganha {} salarios minimos.".format(nome, round(n_trab*valor_hora/998, 2)))
+
+
+# for i in range(500):
+#     print(i)
+
+
+usu = "pirigotica"
+sen = 1234
+
+i=0
+
+while i !=3 :
+    usuario = input("Digite seu usuário linda: \n")
+    senha = input("Digite sua senha linda: \n")
+    if usu == usuario and int(senha)==sen: 
+        print("Passou linda!")
+    else:
+        print("Errou otária!")
+    i += 1
+if i ==3:
+    print("Excedeu o número de tentativas otáriah!")
